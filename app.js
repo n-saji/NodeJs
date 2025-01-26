595e06f946a5750b363ae3b1408b30606e0bb575
Nikhil Saji <nikhilsaji200@gmail.com>
2025-01-26 17:40:16 -0500

    NodeJS initial commit

---
+++ app.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const PORT = process.env.PORT || 5050;
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+app.use(express.json());
+
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://n-saji.github.io"],
+  })
+);
+
+app.get("/api/weather", async (req, res) => {
+  try {
+    const apiKey = process.env.API_KEY || "63fa1a019632987a45748e935162fc0d";
+    const WEATHER_API_URL =
+      process.env.WEATHER_API_URL || "https://api.openweathermap.org/data/2.5";
+    const { lat, lon } = req.query;
+    const apiUrl = `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+
+    const response = await axios.get(apiUrl, {
+      headers: { Authorization: `Bearer ${apiKey}` },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error making API call:", error.message);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.get("/api/cities", async (req, res) => {
+  try {
+    const apiurl = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+    const { city } = req.query;
+    const apiUrl = apiurl + `/cities?namePrefix=${city}`;
+    const RAPID_API_KEY =
+      process.env.RAPID_API_KEY ||
+      "c6ef564e4fmsh4cab87d4be73e1fp1f5038jsn1e0a1fc7d18f";
+    console.log(apiUrl);
+
+    const response = await axios.get(apiUrl, {
+      headers: {
+        "x-rapidapi-key": `${RAPID_API_KEY}`,
+        "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
+      },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error making API call:", error.message);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
